b891cc05916c3d9ea112ea46e4dcf1ec8cae927e
penneytech <99091373+penneytech@users.noreply.github.com>
2024-11-12 09:05:05 -0500

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+/* 
+To gain experience using objects, please complete the following:
+
+1) You will fetch an object from an API url (posted on our classroom wall). 
+2) Console log the result of the API fetch.
+3) Explore the object in the console to learn about it's structure and the data contained.
+4) Create a small user interface that displays the data returned in the object. 
+   Use flexbox and your other HTML, CSS skills to make the interface look good.
+
+*/
+
+
+
